070a0a0f32a3973c339849b86409250233bc895c
leozhao <andrewzhaolei@gmail.com>
2017-01-02 12:53:57 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,16 +9,14 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
        @group = Group.find(params[:id])
      end
      def edit
+    end
 
-           end
-      end
     def new
     @group = Group.new
     end
     def create
       @group = Group.new(group_params)
       @group.user = current_user
-
       if @group.save
      redirect_to groups_path
    else
@@ -26,18 +24,17 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
    end
      end
      def update
-
-
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
       render :edit
     end
        end
-       def destroy
+
+  def destroy
    @group.destroy
    redirect_to groups_path, alert: "Group deleted"
-         end
+  end
      private
      def find_group_and_check_permission
          @group = Group.find(params[:id])
